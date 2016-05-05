e2d59b6d7a1fc7663a4a1cc2e933144bbca2f43c
ahmad aghazadeh <ahmad.aghazadeh.a@gmail.com>
2016-05-05 12:24:53 +0430

    First commit

---
+++ src/ConvertAnsiToUtf8/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ConvertAnsiToUtf8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.ebExt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnStart.Location = new System.Drawing.Point(219, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(105, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Convert";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ebExt
+            // 
+            this.ebExt.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ebExt.Location = new System.Drawing.Point(71, 11);
+            this.ebExt.Name = "ebExt";
+            this.ebExt.Size = new System.Drawing.Size(41, 20);
+            this.ebExt.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Extension";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 39);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(312, 15);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 66);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ebExt);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Convert Ansi To UTF8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox ebExt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ src/ConvertAnsiToUtf8/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertAnsiToUtf8
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            progressBar1.Value = 0;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            
+            DialogResult result = fbd.ShowDialog();
+
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+     
+                String[] files = System.IO.Directory.GetFiles(fbd.SelectedPath, "*."+ ebExt.Text.Trim().ToLower(), System.IO.SearchOption.AllDirectories);
+
+                progressBar1.Maximum = files.Length;
+                int index = 1;
+                foreach (var file in files)
+                {
+                       
+                        byte[] ansiBytes;
+                        using (var reader = new System.IO.StreamReader(file, true))
+                        {
+                                 ansiBytes = File.ReadAllBytes(file);
+                        }
+                        if (!IsUTF8Bytes(ansiBytes))
+                        {
+                            System.IO.File.Move(file, file + "_");
+                            var utf8String = Encoding.Default.GetString(ansiBytes);
+                            File.WriteAllText(file, utf8String);
+                        }
+                
+                    Application.DoEvents();
+                    progressBar1.Value = index++;
+                }
+
+            }
+
+            btnStart.Enabled = true;
+        }
+
+
+
+
+        public static System.Text.Encoding GetType(FileStream fs)
+        {
+            byte[] Unicode = new byte[] { 0xFF, 0xFE, 0x41 };
+            byte[] UnicodeBIG = new byte[] { 0xFE, 0xFF, 0x00 };
+            byte[] UTF8 = new byte[] { 0xEF, 0xBB, 0xBF }; //with BOM
+            Encoding reVal = Encoding.Default;
+
+            BinaryReader r = new BinaryReader(fs, System.Text.Encoding.Default);
+            int i;
+            int.TryParse(fs.Length.ToString(), out i);
+            byte[] ss = r.ReadBytes(i);
+            if (IsUTF8Bytes(ss) || (ss[0] == 0xEF && ss[1] == 0xBB && ss[2] == 0xBF))
+            {
+                reVal = Encoding.UTF8;
+            }
+            else if (ss[0] == 0xFE && ss[1] == 0xFF && ss[2] == 0x00)
+            {
+                reVal = Encoding.BigEndianUnicode;
+            }
+            else if (ss[0] == 0xFF && ss[1] == 0xFE && ss[2] == 0x41)
+            {
+                reVal = Encoding.Unicode;
+            }
+            r.Close();
+            return reVal;
+
+        }
+
+        private static bool IsUTF8Bytes(byte[] data)
+        {
+            int charByteCounter = 1;
+            byte curByte;
+            for (int i = 0; i < data.Length; i++)
+            {
+                curByte = data[i];
+                if (charByteCounter == 1)
+                {
+                    if (curByte >= 0x80)
+                    {
+                        while (((curByte <<= 1) & 0x80) != 0)
+                        {
+                            charByteCounter++;
+                        }
+
+                        if (charByteCounter == 1 || charByteCounter > 6)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    if ((curByte & 0xC0) != 0x80)
+                    {
+                        return false;
+                    }
+                    charByteCounter--;
+                }
+            }
+            if (charByteCounter > 1)
+            {
+                throw new Exception("Error byte format");
+            }
+            return true;
+        }
+
+    }
+
+}
